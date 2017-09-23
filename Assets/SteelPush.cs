289e9c262b8ecdde6c1736e6216de3a32206ca40
Lucas Penney <lucaspenney@gmail.com>
2017-09-23 11:14:16 -0700

    Editor updates

--- Assets/SteelPush.cs
+++ Assets/SteelPush.cs
@@ -3,16 +3,33 @@ using System.Collections;
 
 public class SteelPush : MonoBehaviour {
 
+	public GameObject controller;
+
 	// Use this for initialization
 	void Start() {
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update() {
-		if(Input.GetMouseButton(0)) {
-			GameObject[] objs = GameObject.FindObjectsOfType<GameObject>();
+		if(Input.GetMouseButton(0) || Input.GetMouseButton(1)) {
+			Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, -0.1f));
+			float dist = 100f;
+			float radius = 0.05f;
 
+			RaycastHit[] hits;
+			hits = Physics.SphereCastAll(ray, radius, dist);
+			for(int i = 0;i < hits.Length;i++) {
+				Rigidbody r = hits[i].collider.gameObject.transform.root.GetComponentInChildren<Rigidbody>();
+				if(r != null) {
+					if(Input.GetMouseButton(0)) {
+						r.AddForceAtPosition(Camera.main.transform.forward * 50, hits[i].point);
+					}
+					if(Input.GetMouseButton(1)) {
+						r.AddForceAtPosition(Camera.main.transform.forward * -50, hits[i].point);
+					}
+				}	
+			}
 		}
 	}
 }
